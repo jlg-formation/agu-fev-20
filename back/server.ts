4382000db11be9c7f37d316cc669a3aeafeb522f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-02-18 15:27:35 +0100

    convert back to ts

--- back/server.ts
+++ back/server.ts
@@ -2,8 +2,7 @@ const express = require('express');
 const serveIndex = require('serve-index');
 const app = express();
 
-const www = '../front/dist/front';
-
+let www = '../front/dist/front';
 app.use(express.static(www));
 app.use(serveIndex(www, {'icons': true}));
 
